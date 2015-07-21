4c3d1725d73b80762a84859f97ef727e9718af6c
Benjie Jiao <benjiao12@gmail.com>
2015-07-22 04:37:15 +0800

    Working create task endpoint

--- app/modules/tasks/routes.py
+++ app/modules/tasks/routes.py
@@ -2,6 +2,7 @@ import json
 from flask import Blueprint
 from flask import current_app
 from flask import make_response
+from flask import request
 from datetime import datetime
 
 
@@ -12,41 +13,90 @@ mod_tasks = Blueprint('mod_tasks', __name__)
 def createTask():
     """
     Creates a new task
+
+    Sample Request:
+
+    Headers:
+        Accept: application/vnd.api+json
+        Content-Type: application/vnd.api+json
+
+    Body:
+        {
+            "data": {
+                "attributes": {
+                    "scheduled_time": "2015-07-12 00:00:00",
+                    "endpoint_body": "{\"name\": \"Benjie\"}",
+                    "endpoint_headers": {
+                        "Content-Type": "application/json"
+                    },
+                    "endpoint_method": "POST",
+                    "endpoint_url": "http://example.com/endpoint/",
+                    "max_retry_count": 2
+                },
+                "type": "task"
+            }
+        }
     """
 
-    current_app.logger.info("Create Task Called!")
+    try:
+        data = json.loads(request.data)
+        attributes = data["data"]["attributes"]
+    except:
+        return make_response("Invalid Data Format!", 400)
 
-    data = {"scheduled_time": datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
-            "endpoint_url": "http://test.com/test",
-            "endpoint_headers": {
-                "Authentication": "Test:Testing"
-            },
-            "endpoint_body": "Test Body",
-            "endpoint_method": "POST",
-            "max_retry_count": 5}
+    current_app.logger.info("Data: %s", json.dumps(data, indent=4))
 
-    current_app.tasks.createTask(
-        scheduled_time=data["scheduled_time"],
-        endpoint_url=data["endpoint_url"],
-        endpoint_headers=data["endpoint_headers"],
-        endpoint_body=data["endpoint_body"],
-        endpoint_method=data["endpoint_method"],
-        max_retry_count=data["max_retry_count"])
+    # Check required fields before moving on
+    try:
+        scheduled_time = attributes["scheduled_time"]
+        endpoint_url = attributes["endpoint_url"]
+        endpoint_method = attributes["endpoint_method"]
+    except:
+        response = make_response("Required parameters might be missing", 403)
+        return response
+
+    endpoint_headers = attributes.get("endpoint_headers")
+    endpoint_body = attributes.get("endpoint_body")
+    max_retry_count = attributes.get("max_retry_count", 5)
+
+    # Check if timestamp is in correct format
+    try:
+        scheduled_time_dt = datetime.strptime(scheduled_time, "%Y-%m-%d %H:%M:%S")
+    except:
+        response = make_response("Timestamps should be in YYYY-MM-DD HH:MM:SS format", 403)
+        return response
+
+    # Proceed with creating a task
+    task_uuid = current_app.tasks.createTask(
+        scheduled_time=scheduled_time_dt,
+        endpoint_url=endpoint_url,
+        endpoint_headers=endpoint_headers,
+        endpoint_body=endpoint_body,
+        endpoint_method=endpoint_method,
+        max_retry_count=max_retry_count)
+
+    current_app.logger.info("Task Created: %s", task_uuid)
 
     response_body = {
         "data": {
-            "type": "task",
+            "type": "tasks",
             "attributes": {
-                "time": "2015-03-10 00:00:00"
+                "id": task_uuid,
+                "scheduled_time": scheduled_time,
+                "endpoint_url": endpoint_url,
+                "endpoint_headers": endpoint_headers,
+                "endpoint_body": endpoint_body,
+                "endpoint_method": endpoint_method,
+                "max_retry_count": max_retry_count
             },
             "links": [
-                'http://api.reach-social.com/v1.0/1'
+                'http://localhost/tasks/%s' % task_uuid
             ]
         }
     }
 
     response = make_response(json.dumps(response_body), 201)
-    response.headers['Location'] = 'http://api.reach-social.com/gnip/v1.0/1'
+    response.headers['Location'] = 'http://localhost/tasks/'
     response.headers['Content-Type'] = 'application/vnd.api+json'
     return response
 
@@ -65,7 +115,7 @@ def getTasks():
                 "time": "2015-03-10 00:00:00"
             },
             "links": [
-                'http://api.reach-social.com/v1.0/1'
+                'http://localhost/tasks/'
             ]
         }
     }
@@ -90,7 +140,7 @@ def getTask(task_id):
                 "time": "2015-03-10 00:00:00"
             },
             "links": [
-                'http://api.reach-social.com/v1.0/1'
+                'http://localhost/tasks/'
             ]
         }
     }
