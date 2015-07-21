9bded7e2044a4d2586767c3c86eb1a74c8d3e256
Benjie Jiao <benjiao12@gmail.com>
2015-07-22 03:05:02 +0800

    Added logging

--- app/app.py
+++ app/app.py
@@ -1,11 +1,27 @@
 #!flask/bin/python
 from flask import Flask
+from tasks import TaskLogic
 from modules.tasks import mod_tasks
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
+
+engine = create_engine('sqlite:///db/test.db', echo=False)
+
+# Add application logic to flask app (?)
+app.tasks = TaskLogic(db_engine=engine)
 
 app.register_blueprint(mod_tasks, url_prefix="/tasks")
 
-app.run(host='0.0.0.0', debug=False)
+if __name__ == '__main__':
+    import logging
+    from logging.handlers import RotatingFileHandler
+
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(message)s')
+
+    handler = RotatingFileHandler('foo.log')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.DEBUG)
+    app.logger.addHandler(handler)
+
+    app.run(host='0.0.0.0', debug=True)
--- app/modules/tasks/routes.py
+++ app/modules/tasks/routes.py
@@ -1,15 +1,37 @@
 import json
 from flask import Blueprint
+from flask import current_app
 from flask import make_response
+from datetime import datetime
+
 
 mod_tasks = Blueprint('mod_tasks', __name__)
 
 
-@mod_tasks.route("/", methods=["GET"])
-def getTasks():
+@mod_tasks.route("/", methods=["POST"])
+def createTask():
     """
-    Returns a list of all tasks
+    Creates a new task
     """
+
+    current_app.logger.info("Create Task Called!")
+
+    data = {"scheduled_time": datetime.strptime("2020-01-01 00:00:00", "%Y-%m-%d %H:%M:%S"),
+            "endpoint_url": "http://test.com/test",
+            "endpoint_headers": {
+                "Authentication": "Test:Testing"
+            },
+            "endpoint_body": "Test Body",
+            "endpoint_method": "POST",
+            "max_retry_count": 5}
+
+    current_app.tasks.createTask(
+        scheduled_time=data["scheduled_time"],
+        endpoint_url=data["endpoint_url"],
+        endpoint_headers=data["endpoint_headers"],
+        endpoint_body=data["endpoint_body"],
+        endpoint_method=data["endpoint_method"],
+        max_retry_count=data["max_retry_count"])
 
     response_body = {
         "data": {
@@ -29,12 +51,29 @@ def getTasks():
     return response
 
 
-@mod_tasks.route("/", methods=["POST"])
-def createTask():
+@mod_tasks.route("/", methods=["GET"])
+def getTasks():
     """
-    Creates a new task
+    Returns a list of all tasks
     """
-    return json.dumps({"data": ""})
+    current_app.logger.info("Get Tasks Called!")
+
+    response_body = {
+        "data": {
+            "type": "task",
+            "attributes": {
+                "time": "2015-03-10 00:00:00"
+            },
+            "links": [
+                'http://api.reach-social.com/v1.0/1'
+            ]
+        }
+    }
+
+    response = make_response(json.dumps(response_body), 200)
+    response.headers['Location'] = 'http://api.reach-social.com/gnip/v1.0/1'
+    response.headers['Content-Type'] = 'application/vnd.api+json'
+    return response
 
 
 @mod_tasks.route("/<task_id>", methods=["GET"])
@@ -42,15 +81,32 @@ def getTask(task_id):
     """
     Returns details about a specific task
     """
+    current_app.logger.info("Get Task Called!")
 
-    return json.dumps({"data": ""})
+    response_body = {
+        "data": {
+            "type": "task",
+            "attributes": {
+                "time": "2015-03-10 00:00:00"
+            },
+            "links": [
+                'http://api.reach-social.com/v1.0/1'
+            ]
+        }
+    }
+    response = make_response(json.dumps(response_body), 201)
+    response.headers['Location'] = 'http://api.reach-social.com/gnip/v1.0/1'
+    response.headers['Content-Type'] = 'application/vnd.api+json'
+    return response
 
 
 @mod_tasks.route("/<task_id>", methods=["DELETE"])
 def deleteTask(task_id):
+    current_app.logger.info("Delete Task Called!")
     return json.dumps({"data": ""})
 
 
 @mod_tasks.route("/<task_id>", methods=["PATCH"])
 def updateTask(task_id):
+    current_app.logger.info("Update Task Called!")
     return json.dumps({"data": ""})
