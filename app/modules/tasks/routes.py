7afa50236dc1b8b2a2bdc9193c41fb459d347cf9
Benjie Jiao <benjiao12@gmail.com>
2015-07-21 20:16:57 +0800

    Added dummy endpoints for /tasks

--- app/modules/tasks/routes.py
+++ app/modules/tasks/routes.py
@@ -5,8 +5,12 @@ from flask import make_response
 mod_tasks = Blueprint('mod_tasks', __name__)
 
 
-@mod_tasks.route("/", methods=["POST"])
-def createTask():
+@mod_tasks.route("/", methods=["GET"])
+def getTasks():
+    """
+    Returns a list of all tasks
+    """
+
     response_body = {
         "data": {
             "type": "task",
@@ -23,3 +27,30 @@ def createTask():
     response.headers['Location'] = 'http://api.reach-social.com/gnip/v1.0/1'
     response.headers['Content-Type'] = 'application/vnd.api+json'
     return response
+
+
+@mod_tasks.route("/", methods=["POST"])
+def createTask():
+    """
+    Creates a new task
+    """
+    return json.dumps({"data": ""})
+
+
+@mod_tasks.route("/<task_id>", methods=["GET"])
+def getTask(task_id):
+    """
+    Returns details about a specific task
+    """
+
+    return json.dumps({"data": ""})
+
+
+@mod_tasks.route("/<task_id>", methods=["DELETE"])
+def deleteTask(task_id):
+    return json.dumps({"data": ""})
+
+
+@mod_tasks.route("/<task_id>", methods=["PATCH"])
+def updateTask(task_id):
+    return json.dumps({"data": ""})
